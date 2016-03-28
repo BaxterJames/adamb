4e7339578d89e669229080becc8f2165bf4b23ea
sammcgrail <sammcgrail@gmail.com>
2016-03-27 22:43:50 -0400

    init file

---
+++ py.py
@@ -0,0 +1,9 @@
+print "enter your name"
+
+person = raw_input()
+
+print("Your name is " + person + " bitch")
+
+namelength = len(person) * 4
+
+print ("Number of letters in name * 4 is " + str(namelength) + " letters")
